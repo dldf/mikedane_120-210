427c2eabe9b8652d00d0f2f4da568ac93c88b072
Daniel DeFrance <daniel.defrance@montana.edu>
2022-02-20 15:53:40 -0700

    Add Initial Searchbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './SearchBar/SearchBar';
 
 const App = () => {
     return (
         <div>
-            <p>setup</p>
+            <SearchBar />
         </div>
     );
 }
