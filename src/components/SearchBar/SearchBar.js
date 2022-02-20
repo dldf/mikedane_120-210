e945f2406d337423613c76f340225bd3a93d0244
Daniel DeFrance <daniel.defrance@montana.edu>
2022-02-20 16:54:44 -0700

    add search functionality

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
+
+const products = [
+    'bookplates',
+    'bookends',
+    'paste',
+    'magnets',
+    'book care'
+]
 
 const SearchBar = () => {
+    const [searchValue, setsearchValue] = useState("")
+    const handleInputChange = (e) => {
+        setsearchValue(e.target.value)
+    }
+    const handleClearClick = () => {
+        setsearchValue('')
+    }
+    const filteredProducts = products.filter((product) => {
+        return product.includes(searchValue)
+    }
+    )
+    const shouldDisplayClearButton = searchValue.length > 0
     return (
         <div>
-            Search Bar
+            <input type="text"
+                value={searchValue}
+                onChange={handleInputChange}
+                placeholder="Enter Search..." />
+            {shouldDisplayClearButton &&
+                <button onClick={handleClearClick}>Clear</button>}
+
+            <ul>
+                {filteredProducts.map((product) => (
+                    <li key={product}>{product}</li>
+                ))}
+            </ul>
         </div>
     )
 }
